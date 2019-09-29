8c493cc1337bd58efca22b98e06672c40d989a20
MichaelChoudhury <MichaelChoudhury@users.noreply.github.com>
2019-09-29 20:40:15 +1000

    update emotions
    update emotions

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = ''https://www.dropbox.com/sh/qlqphgkuqrtqi0z/AABsvNOjUeSZ0K2JP6LPqufja?dl=0''
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['happy', 'sad', 'angry']
 path = Path(__file__).parent
 
 app = Starlette()
