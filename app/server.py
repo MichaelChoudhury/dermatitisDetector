77cd553c2c8ce6b87d4e08535ef512380982b5e5
MichaelChoudhury <MichaelChoudhury@users.noreply.github.com>
2019-10-03 21:04:16 +1000

    update main.py

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://www.dropbox.com/s/j8bvf35768lhpfk/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
-classes = ['happy', 'sad', 'angry']
+classes = ['eczema', 'measles', 'melanoma']
 path = Path(__file__).parent
 
 app = Starlette()
